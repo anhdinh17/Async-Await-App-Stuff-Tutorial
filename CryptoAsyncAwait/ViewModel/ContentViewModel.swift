24589ef8bd8e1a9ea71b2f728ab477ce9b529d83
Anh Dinh <anhdinh17@gmail.com>
2024-05-02 11:34:07 -0500

    Async Await networking

--- CryptoAsyncAwait/ViewModel/ContentViewModel.swift
+++ CryptoAsyncAwait/ViewModel/ContentViewModel.swift
@@ -17,7 +17,31 @@ class ContentViewModel: ObservableObject {
     }
     
     init() {
-        fetchCoinsWithURLSession()
+        loadData()
+    }
+}
+//MARK: - Async/Await
+extension ContentViewModel {
+    // mark this func @MainActor to let compiler know
+    // this func is working on main thread
+    // This is equal to the block DispatchQueue.main.async{}
+    @MainActor
+    func fetchCoinsAsync() async throws {
+        guard let url = URL(string: urlString) else {
+            print("DEBUG: Invalid URL")
+            return
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { return }
+        guard let coins = try? JSONDecoder().decode([Coin].self, from: data) else { return }
+        self.coins = coins
+    }
+    
+    func loadData() {
+        Task {
+            try await fetchCoinsAsync()
+        }
     }
 }
 
